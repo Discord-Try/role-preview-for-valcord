e8a79f51918ba8d1c7d64437f8e0b939b99b0b21
Lily Wonhalf <lilywonhalf@gmail.com>
2023-10-21 05:29:00 +0200

    Unicode characters support (thanks zziger)

--- src/saveableState.ts
+++ src/saveableState.ts
@@ -1,19 +1,20 @@
 import type { Role } from "./Role";
 import { getHashVars, setHashVars } from "./hashVars";
+import { b64DecodeUnicode, b64EncodeUnicode } from "./utils";
 
 export interface SaveableState {
-  roles: Role[];
-  colorBlindModes: string[];
+    roles: Role[];
+    colorBlindModes: string[];
 }
 
 export function saveState(state: SaveableState) {
-  const encoded = btoa(JSON.stringify(state));
-  const hashVars = getHashVars();
-  hashVars.state = encoded;
-  setHashVars(hashVars);
+    const encoded = b64EncodeUnicode(JSON.stringify(state));
+    const hashVars = getHashVars();
+    hashVars.state = encoded;
+    setHashVars(hashVars);
 }
 
 export function loadState(): SaveableState | null {
-  const hashVars = getHashVars();
-  return hashVars.state ? JSON.parse(atob(hashVars.state)) : null;
+    const hashVars = getHashVars();
+    return hashVars.state ? JSON.parse(b64DecodeUnicode(hashVars.state)) : null;
 }
--- src/utils.ts
+++ src/utils.ts
@@ -1,14 +1,33 @@
 import { useEffect, useRef } from "react";
 
 export function useEffectIgnoreFirstCall(effect: any, deps: any[]) {
-  const first = useRef(true);
+    const first = useRef(true);
 
-  useEffect(() => {
-    if (first.current) {
-      first.current = false;
-      return;
-    }
+    useEffect(() => {
+        if (first.current) {
+            first.current = false;
+            return;
+        }
 
-    effect();
-  }, deps);
+        effect();
+    }, deps);
+}
+
+export function b64EncodeUnicode(str: string) {
+    return btoa(
+        encodeURIComponent(str).replace(/%([0-9A-F]{2})/g, function toSolidBytes(match, p1) {
+            return String.fromCharCode(parseInt("0x" + p1));
+        }),
+    );
+}
+
+export function b64DecodeUnicode(str: string) {
+    return decodeURIComponent(
+        atob(str)
+            .split("")
+            .map(function (c) {
+                return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+            })
+            .join(""),
+    );
 }
